66c6ec3b18da6a194ecb6467dd2fdea67a9a94c5
Kellie Banzon <kelliebanzon@gmail.com>
2016-11-14 12:00:50 -0800

    Comments 01
    Beginning to elaborate on the design recipe (data definitions,
    signatures, and check-expects). Still incomplete, more to come.

--- FinalProject.rkt
+++ FinalProject.rkt
@@ -5,63 +5,87 @@
 (require 2htdp/universe)
 (require 2htdp/image)
 
-;;More dirty hacks
+;; More dirty hacks
 (define (both a b) b)
 
-;;A markov chain has
-;;A List of nodes in the chain AND a integer value corresponding to the index of the list where the markov chain is currently at
+;; A markov-node has
+;; - midi: a integer value that corresponds to a MIDI note number
+;; - connections: a list of connection strengths, where the strengths are numbers between 0.0 and 1.0
+(define-struct markov-node [midi connections])
+
+;; A markov-chain has
+;; - nodes: a list of markov-nodes in the markov-chain
+;; - current-node: a integer value corresponding to the current index of the markov-chain list
 (define-struct markov-chain [nodes current-node])
 
-;;A Markov Node has
-;;A value of the node that corresponds to a MIDI note number AND a list of connection strenghts
-(define-struct markov-node [value connections])
-
-;;This is used to add a node to a chain
+;; Add a markov-node to a markov-chain
+;; node: the markov-node to be added
+;; chain: the markov-chain to add the markov-node to
+;; markov-node markov-chain -> markov-chain
+;; --------------- ADD CHECK-EXPECTS ---------------
 (define (add-node-to-chain node chain)
   (make-markov-chain (append (alert-all-nodes-add (markov-chain-nodes chain)) (list node)) (markov-chain-current-node chain))
   )
 
-;;Chain -> Chain with updated nodes
-;;Alert all nodes of a new node
+;; Alert all nodes of a new node
+;; list: the nodes of a markov-chain
+;; markov-chain-nodes -> markov-chain-nodes (with updated nodes)
+;; --------------- ADD CHECK-EXPECTS ---------------
 (define (alert-all-nodes-add list)
   (cond
     [(empty? list) '()]
     [else (cons (node-alert-chain-add (first list)) (alert-all-nodes-add (rest list)))]))
 
 
-;;This is called on all of the nodes in the chain when another node is added
-;; Markov Node -> Markov Node
+;; Add a connection to each of the markov-nodes in the markov-chain
+;; Adds a new connection to the markov-node, then normalizes all of that markov-node's connections
+;; node: the markov-node whose connections are being adjusted
+;; markov-node -> markov-node
+;; --------------- ADD CHECK-EXPECTS --------------- (note: how do you check the code when it calls for a random value?)
 (define (node-alert-chain-add node)
-  (make-markov-node (markov-node-value node)
+  (make-markov-node (markov-node-midi node)
                     (normalize-node (append (markov-node-connections node) (list (random)))))
   )
 
-;;Takes a list of connection strengths and returns a normalized list of connection strengths
-;; List of Numbers -> List of Numbers (whose sum is 1)
+;; Takes a list of connection strengths and returns a normalized list of connection strengths
+;; connections: a list of numbers between 0.0 and 1.0 that denote the connection strengths of a markov-node
+;; List-of-Numbers -> List-of-Numbers (whose sum is 1)
+(check-expect (normalize-node (cons 1.0 '())) (cons 1.0 '()))
+(check-expect (normalize-node (cons 0.2 (cons 0.3 '()))) (cons 0.4 (cons 0.6 '())))
+(check-expect (normalize-node (cons 0.4 (cons 0.6 (cons 0.4 (cons 0.6 '()))))) (cons 0.2 (cons 0.3 (cons 0.2 (cons 0.3 '())))))
 (define (normalize-node connections)
   (get-normalized connections (sum-of-list connections)))
 
+;; normalizes a list of connections by dividing each value by the sum of the list
+;; essentially converts each value to the percentage of the whole that it represents
+;; connections: a list of numbers between 0.0 and 1.0 that denote the connection strengths of a markov-node
+;; sum: the sum of the values in connections
+;; list-of-numbers decimal-number -> list-of-numbers (whose sum is 1)
+(check-expect (get-normalized (cons 1.0 '()) 1.0) (cons 1.0 '()))
+(check-expect (get-normalized (cons 0.2 (cons 0.3 '())) 0.5) (cons 0.4 (cons 0.6 '())))
+(check-expect (get-normalized (cons 0.4 (cons 0.6 (cons 0.4 (cons 0.6 '())))) 2.0) (cons 0.2 (cons 0.3 (cons 0.2 (cons 0.3 '())))))
 (define (get-normalized connections sum)
   (cond
     [(empty? connections) '()]
     [else (cons (/ (first connections) sum) (get-normalized (rest connections) sum))]))
 
-;; Gets the sum of all elements in a list
-;; List of Numbers -> Number
+;; computes the sum of all elements in a list
+;; list: a list of numbers
+;; List-of-Numbers -> Number
 (define (sum-of-list list)
   (cond
     [(empty? list) 0]
     [else (+ (first list) (sum-of-list (rest list)))]
     ))
 
-;;Takes a chain and returns the same chain with current-node changed to be the new one
-;;Markov Chain -> Number
+;; Takes a chain and returns the same chain with current-node changed to be the new one
+;; Markov Chain -> Number
 (define (get-next-node chain)
   (pick-node-based-on-weights (markov-node-connections (list-ref (markov-chain-nodes chain) (markov-chain-current-node chain))) 0 (random))
   )
 
-;;Call this with the node's weights, 0 and a random floating point number in 0 < rand < 1
-;;Number List, 0, 0 < rand < 1 -> Number
+;; Call this with the node's weights, 0 and a random floating point number in 0 < rand < 1
+;; Number List, 0, 0 < rand < 1 -> Number
 (define (pick-node-based-on-weights weights inc rand)
   (cond
     [(empty? weights) 0]
@@ -85,7 +109,7 @@
   (add-node-to-chain (make-markov-node 1 '(.5 .5))
   (add-node-to-chain (make-markov-node 0 '(1)) (make-markov-chain '() 0))))))
 
-;;The world state of this big bang is a Markov Chain
+;; The world state of this big bang is a Markov Chain
 (big-bang initial-chain
           [to-draw draw-handler]
           [on-tick tick-handler 1/5])
